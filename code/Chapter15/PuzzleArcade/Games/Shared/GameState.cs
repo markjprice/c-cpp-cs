5c7bd52721742af27858145b9f30bbfb27d6eabb
Mark J. Price <markjprice@gmail.com>
2026-06-16 12:42:24 +0100

    Add code solution for Ch15

---
+++ code/Chapter15/PuzzleArcade/Games/Shared/GameState.cs
@@ -0,0 +1,11 @@
+﻿namespace PuzzleArcade.Games.Shared;
+
+public sealed record class NotStarted;
+
+public sealed record class Playing(int TurnsTaken);
+
+public sealed record class Won(int TurnsTaken);
+
+public sealed record class Lost(string Reason);
+
+public union GameState(NotStarted, Playing, Won, Lost);
